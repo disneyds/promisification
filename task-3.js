2b9ae16897417be6e2376ff507c67eb9ffbca5e0
Aleksandr Chernishov <caa190993@gmail.com>
2020-11-09 14:22:51 +0200

    fixed task-3

--- task-3.js
+++ task-3.js
@@ -11,7 +11,7 @@ const makeTransaction = (transaction) => {
     const canProcess = Math.random() > 0.3;
 
     if (canProcess) {
-      resolve(transaction.id, delay);
+      resolve({id: transaction.id, time: delay});
     } else {
       reject(transaction.id);
     }
@@ -20,8 +20,8 @@ const makeTransaction = (transaction) => {
      })
 };
 
-const logSuccess = (id, time) => {
-  console.log(`Transaction ${id} processed in ${time}ms`);
+const logSuccess = ({ id, time }) => {
+  console.log(`Transaction ${id} processed in ${time} ms`);
 };
 
 const logError = id => {
